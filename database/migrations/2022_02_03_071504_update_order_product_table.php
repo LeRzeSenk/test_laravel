2c805144b229429e45f63ed9ad05b20ae63ff61d
LeRzeSenk <festoplay@gmail.com>
2022-02-03 12:19:21 +0500

    //

--- database/migrations/2022_02_03_071504_update_order_product_table.php
+++ database/migrations/2022_02_03_071504_update_order_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderProductTable extends Migration
+class UpdateOrderProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateOrderProductTable extends Migration
     public function up()
     {
         Schema::table('order_product', function (Blueprint $table) {
-            $table->integer('count')->default(1);
+            $table->integer('count')->default(1)->after('product_id');
         });
     }
 
