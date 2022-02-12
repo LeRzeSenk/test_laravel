76aba858554d1a67405ebe512822433e48ebbc5e
LeRzeSenk <festoplay@gmail.com>
2022-02-12 12:25:48 +0500

    //

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -45,7 +45,7 @@
                     <div class="input-group row">
                         <label for="category_id" class="col-sm-4 col-form-label">Категория: </label>
                         <div class="col-sm-6">
-                            <select name="category_id" id="category_id">
+                            <select name="category_id" id="category_id" class="form-control">
                                 @foreach($categories as $category)
                                 <option @isset($product) @if($product->category->id == $category->id) selected @endif @endisset
                                 value="{{$category->id}}">{{$category->name}}</option>
